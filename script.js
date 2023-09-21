55bab09902a2aa9aa156c9417ea2c97426185ef9
darshanhu <133042615+darshanhu@users.noreply.github.com>
2023-09-21 22:55:52 +0530

    updated ID

---
+++ script.js
@@ -0,0 +1,14 @@
+// Iframe that sends the click event
+document.getElementById('first-iframe').addEventListener('click', function() {
+      // Send a message to the other iframe with the ID of the element to click
+      window.frames['second-iframe'].postMessage(document.getElementById('softland-btn').id, '*');
+    });
+    
+    // Iframe that receives the click event
+    window.addEventListener('message', function(event) {
+      // Get a reference to the element to click
+      var element = document.getElementById(event.data);
+    
+      // Click the element
+      element.click();
+    });
